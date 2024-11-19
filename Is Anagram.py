63aea163fbfee283861be1d14b004672efa7ccf0
Muhammad Osman <serenitystream7@gmail.com>
2024-11-19 21:30:57 +0500

    Add files via upload

---
+++ Is Anagram.py
@@ -0,0 +1,45 @@
+# Given two strings s and t, return true if t is an 
+# anagram
+#  of s, and false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+
+# Output: true
+
+# Example 2:
+
+# Input: s = "rat", t = "car"
+
+# Output: false
+
+
+# Anagram: Strings that contain the same characters with the same frequencies.
+
+# First loop: Counts character frequencies in s.
+
+# Second loop: Counts character frequencies in t.
+
+# Comparison: If the dictionaries are equal, the strings are anagrams (True); otherwise, they are not (False).
+
+
+
+class Solution:
+    class Anagram:
+        def isanagram(s,t):
+            if len(s) != len(t):
+                return False
+            countS, countT = {}, {}
+            
+            for char in s:
+                countS[char] = countS.get(char,0) + 1
+            for char in t:
+                countT[char] = countT.get(char,0) + 1
+
+            return countS == countT
+        
+print(Solution.Anagram.isanagram("anagram","nagaram"))
+print(Solution.Anagram.isanagram("rat","car"))
